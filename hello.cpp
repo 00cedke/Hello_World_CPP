acd4eadbce1f969cb0409303da6a2a403482996e
cedkeDev123 <123301168+00cedke@users.noreply.github.com>
2023-09-10 02:50:58 +0200

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int char;
+
+int main() {
+    std::cout << "Welcome to my first C++ project !";
+    std::cout << "i love suhsi lol.";
+    return 0;
+}
+
+void hello()
+{
+    cout << "helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo !!!";
+    return 0;
+}
